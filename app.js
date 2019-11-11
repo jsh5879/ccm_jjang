4fd61b65401cefe8ee6d35f1af562c9922b2f986
bjh6654 <bjh6654@naver.com>
2019-11-11 21:29:00 +0900

    Default 경로 변경

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
 const sessionStorage = require('./session.js')
+const open = require('open');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,5 +15,6 @@ app.set('view engine', 'ejs')
 app.engine('html', require('ejs').renderFile)
 
 const server = app.listen(8000, () => {
-    console.log("Express server has started on port 8000")
+    console.log("Express server has started on port 8000");
+    open('http://localhost:8000');
 })
